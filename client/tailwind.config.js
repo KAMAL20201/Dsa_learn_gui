3c1014fb6f5eb0c770ac53f73a9741b643ce12d3
KAMAL20201 <arorakamal706@gmail.com>
2025-11-11 13:37:17 +0530

    Fix: tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class', // Enable class-based dark mode
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
